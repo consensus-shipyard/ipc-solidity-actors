800093fbdd9a341d70e47fe3144c9c4e907b9983
Mihail Kirov <mihail.kirov@limechain.tech>
2023-02-16 12:14:09 +0200

    feat: add subnetID struct

---
+++ src/structs/SubnetId.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+struct SubnetID {
+    /// @notice parent path of the subnet
+    string parent;
+    
+    /// @notice deployed subnet actor address
+    address actor;
+};
