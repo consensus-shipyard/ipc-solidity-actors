badcb369585f8c449b8778a8570974949e436a24
Mihail Kirov <mihail.kirov@limechain.tech>
2023-02-16 12:14:49 +0200

    feat: add SA constructor params struct

---
+++ src/structs/SubnetActorConstructorParams.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import "./SubnetId.sol";
+import "./enums/ConsensusType.sol";
+struct SubentActorConstructorParams {
+    // Parent subnet identifier
+    SubnetID parent;
+
+    // Human-readable name for the subnet
+    string name;
+
+    uint64 ipcGatewayAddr;
+
+    // @notice Consensus implemented in the subnet
+    ConsensusType Consensus;
+
+    /// @notice The minimum stake required to be a validator in this subnet
+    uint256 minValidatorStake;
+
+    // @notice The minimum number of validators required for the subnet
+    uint64 minValidators;
+
+    uint64 finalityThreshold;
+
+    uint64 checkPeriod;
+
+    uint64 genesis;
+}
