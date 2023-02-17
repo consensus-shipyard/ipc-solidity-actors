5e580939f8011ad484b972fe277c8d27e1fe5309
Mihail Kirov <mihail.kirov@limechain.tech>
2023-02-17 11:47:04 +0200

    feat: add deployment test fo SA

--- src/SubnetActor.sol
+++ src/SubnetActor.sol
@@ -5,42 +5,39 @@ import "./enums/Status.sol";
 import "./structs/Checkpoint.sol";
 import "./structs/Subnet.sol";
 import "./structs/Validator.sol";
-import "./structs/SubnetActorConstructorParams.sol";
-
-import "./structs/SubnetActorConstructorParams.sol";
 
 contract SubnetActor {
     /// @notice Human-readable name of the subnet.
-    string private name;
+    string public name;
     /// @notice ID of the parent subnet
-    SubnetID private parentId;
+    SubnetID public parentId;
     /// @notice Address of the IPC gateway for the subnet
-    address private ipcGatewayAddr;
+    address public ipcGatewayAddr;
     /// @notice Type of consensus algorithm.
-    ConsensusType private consensus;
+    ConsensusType public consensus;
     /// @notice The minimum stake required to be a validator in this subnet
-    uint256 private minValidatorStake;
+    uint256 public minValidatorStake;
     /// @notice Total collateral currently deposited in the SCA from the subnet
-    uint256 private totalStake;
+    uint256 public totalStake;
     /// @notice validator address to stake amount
-    mapping(address => uint256) private stake;
+    mapping(address => uint256) public stake;
     /// @notice current status of the subnet
-    Status private status;
+    Status public status;
     /// @notice genesis block number
-    uint64 private genesis;
+    uint64 public genesis;
     /// @notice number of blocks after which finality is reached
-    uint64 private finalityThreshold;
+    uint64 public finalityThreshold;
     /// @notice number of blocks between two checkpoints
-    uint64 private checkPeriod;
+    uint64 public checkPeriod;
     /// @notice block number to corresponding checkpoint at that block
-    mapping(uint64 => Checkpoint) private checkpoints;
+    mapping(uint64 => Checkpoint) public checkpoints;
     /// @notice CID to Votes (list of validators)
     mapping(bytes => address[]) windowChecks;
     /// @notice List of validators in the subnet
-    Validator[] private validatorSet;
+    Validator[] public validatorSet;
     /// @notice Minimal number of validators required for the subnet
     // to be able to validate new blocks.
-    uint64 private minValidators;
+    uint64 public minValidators;
 
     constructor(SubnetID memory _parentId, string memory _name, address _ipcGatewayAddr, ConsensusType _consensus, uint256 _minValidatorStake, uint64 _minValidators, uint64 _finalityThreshold, uint64 _checkPeriod, uint64 _genesis) {
         parentId = _parentId;
--- test/SubnetActor.t.sol
+++ test/SubnetActor.t.sol
@@ -7,13 +7,29 @@ import "../src/SubnetActor.sol";
 
 contract SubnetActorTest is Test {
 
-    SubnetActor subnetActor;
+    SubnetActor sa;
 
     address private constant IPC_GATEWAY_ADDR = address(1024);
     string private constant NETWORK_NAME = "test";
 
     function setUp() public {
         SubnetID memory parentId = SubnetID("/root", address(0));
-        subnetActor = new SubnetActor(parentId, NETWORK_NAME, IPC_GATEWAY_ADDR, ConsensusType.Dummy, 0, 0, 0, 0, 0);
+        sa = new SubnetActor(parentId, NETWORK_NAME, IPC_GATEWAY_ADDR, ConsensusType.Dummy, 10, 10, 10, 10, 10);
+    }
+
+
+    function testDeployment() public view {
+
+        require(keccak256(abi.encodePacked(sa.name())) == keccak256(abi.encodePacked("test")));
+        require(sa.ipcGatewayAddr() == IPC_GATEWAY_ADDR);
+        require(sa.consensus() == ConsensusType.Dummy);
+        require(sa.minValidatorStake() == 10);
+        require(sa.minValidators() == 10);
+        require(sa.finalityThreshold() == 10);
+        require(sa.checkPeriod() == 10);
+        require(sa.genesis() == 10);
+        (string memory parent, address actor) = sa.parentId();
+        require(keccak256(abi.encodePacked(parent)) == keccak256(abi.encodePacked("/root")));
+        require(actor == address(0));
     }
 }
