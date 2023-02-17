e86a0a169421b014875ef728ed4ad5f1fbeeabcc
Anton Rusev <anton.rusev.code@gmail.com>
2023-02-17 15:21:24 +0200

    feat: add SCA interface

--- src/SubnetCoordinatorActor.sol
+++ src/SubnetCoordinatorActor.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.7;
 import "./structs/Checkpoint.sol";
 import "./structs/Postbox.sol";
 import "./enums/Status.sol";
+import "./interfaces/ISubnetCoordinatorActor.sol";
 
-contract SubnetCoordinatorActor {
+contract SubnetCoordinatorActor is ISubnetCoordinatorActor {
     uint64 constant DEFAULT_CHECKPOINT_PERIOD = 10;
     uint256 constant MIN_COLLATERAL_AMOUNT = 1 ether;
     uint256 constant MAX_NONCE = type(uint256).max;
@@ -64,5 +65,106 @@ contract SubnetCoordinatorActor {
             ? _checkpointPeriod
             : DEFAULT_CHECKPOINT_PERIOD;
         appliedBottomUpNonce = MAX_NONCE;
+    }
+
+    /// Register is called by subnet actors to put the required collateral
+    /// and register the subnet to the hierarchy.
+    function register() external {
+        revert("MethodNotImplemented");
+    }
+
+    /// AddStake adds stake to the collateral of a subnet.
+    function addStake() external {
+        revert("MethodNotImplemented");
+    }
+
+    /// Release stake recovers some collateral of the subnet
+    function releaseStake(uint amount) external {
+        revert("MethodNotImplemented");
+    }
+
+    // Kill propagates the kill signal from a subnet actor to unregister it from th
+    /// hierarchy.
+    function kill() external {
+        revert("MethodNotImplemented");
+    }
+
+    /// CommitChildCheck propagates the commitment of a checkpoint from a child subnet,
+    /// process the cross-messages directed to the subnet.
+    function commitChildCheck(bytes memory checkpoint) external {
+        revert("MethodNotImplemented");
+    }
+
+    /// Fund injects new funds from an account of the parent chain to a subnet.
+    ///
+    /// This functions receives a transaction with the FILs that want to be injected in the subnet.
+    /// - Funds injected are frozen.
+    /// - A new fund cross-message is created and stored to propagate it to the subnet. It will be
+    /// picked up by miners to include it in the next possible block.
+    /// - The cross-message nonce is updated
+    function fund(bytes memory subnetId) external {
+        revert("MethodNotImplemented");
+    }
+
+    /// Release creates a new check message to release funds in parent chain
+    ///
+    /// This function burns the funds that will be released in the current subnet
+    /// and propagates a new checkpoint message to the parent chain to signal
+    /// the amount of funds that can be released for a specific address.
+    function release() external {
+        revert("MethodNotImplemented");
+    }
+
+    /// SendCross sends an arbitrary cross-message to other subnet in the hierarchy.
+    ///
+    /// If the message includes any funds they need to be burnt (like in Release)
+    /// before being propagated to the corresponding subnet.
+    /// The circulating supply in each subnet needs to be updated as the message passes through them.
+    ///
+    /// Params expect a raw message without any subnet context (the IPC address is
+    /// included in the message by the actor). Only actors are allowed to send arbitrary
+    /// cross-messages as a side-effect of their execution. For plain token exchanges
+    /// fund and release have to be used.
+    function sendCross(
+        bytes memory toSubnetId,
+        bytes memory crossMsg
+    ) external {
+        revert("MethodNotImplemented");
+    }
+
+    /// ApplyMessage triggers the execution of a cross-subnet message validated through the consensus.
+    ///
+    /// This function can only be triggered using `ApplyImplicitMessage`, and the source needs to
+    /// be the SystemActor. Cross messages are applied similarly to how rewards are applied once
+    /// a block has been validated. This function:
+    /// - Determines the type of cross-message.
+    /// - Performs the corresponding state changes.
+    /// - And updated the latest nonce applied for future checks.
+    function applyMessage(bytes memory crossMsg) external {
+        revert("MethodNotImplemented");
+    }
+
+    /// Whitelist a series of addresses as propagator of a cross net message.
+    /// This is basically adding this list of addresses to the `PostBoxItem::owners`.
+    /// Only existing owners can perform this operation.
+    function whitelistPropagator(
+        uint256 postboxId,
+        address[] memory owners
+    ) external {
+        revert("MethodNotImplemented");
+    }
+
+    function propagate(uint256 postboxId) external {
+        revert("MethodNotImplemented");
+    }
+
+    /// Commit the cross message to storage. It outputs a flag signaling
+    /// if the committed messages was bottom-up and some funds need to be
+    /// burnt or if a top-down message fee needs to be distributed.
+    function commitCrossMessage(
+        bytes memory crossMessage,
+        uint256 feeAmount
+    ) external {
+        revert("MethodNotImplemented");
     }
 }
