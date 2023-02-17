2ef0ec882a2529844738a8d517d37824433557b1
Anton Rusev <anton.rusev.code@gmail.com>
2023-02-17 14:31:35 +0200

    test: check default values

--- test/SubnetCoordinatorActor.t.sol
+++ test/SubnetCoordinatorActor.t.sol
@@ -6,6 +6,10 @@ import "forge-std/console.sol";
 import "../src/SubnetCoordinatorActor.sol";
 
 contract SubnetActorTest is Test {
+    uint64 constant DEFAULT_CHECKPOINT_PERIOD = 10;
+    uint256 constant MIN_COLLATERAL_AMOUNT = 1 ether;
+    uint256 constant MAX_NONCE = type(uint256).max;
+
     SubnetCoordinatorActor sca;
 
     function setUp() public {
@@ -19,5 +23,8 @@ contract SubnetActorTest is Test {
             keccak256(abi.encode(parent)) == keccak256(abi.encode("/root"))
         );
         require(actor == address(0));
+        require(sca.minStake() == MIN_COLLATERAL_AMOUNT);
+        require(sca.checkPeriod() == DEFAULT_CHECKPOINT_PERIOD);
+        require(sca.appliedBottomUpNonce() == MAX_NONCE);
     }
 }
