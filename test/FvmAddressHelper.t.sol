9f0c52350cde8c530827f1e117354169b1d6a8e4
cryptoAtwill <willes.lau@protocol.ai>
2023-07-03 18:47:46 +0800

    fix test

--- test/FvmAddressHelper.t.sol
+++ test/FvmAddressHelper.t.sol
@@ -10,7 +10,7 @@ import {FvmAddress} from "../src/structs/FvmAddress.sol";
 contract FvmAddressHelperTest is Test {
     using FvmAddressHelper for FvmAddress;
 
-    function test_works() public view {
+    function test_works() public pure {
         address addr = 0xeC2804Dd9B992C10396b5Af176f06923d984D90e;
         FvmAddress memory fvmAddr = FvmAddressHelper.from(addr);
         
