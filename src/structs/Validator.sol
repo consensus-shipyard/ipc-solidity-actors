3eda0c4bc3f921b98d0af56fdeaa4563ba1bc799
Denis Kolegov <dnkolegov@gmail.com>
2023-07-11 21:07:47 +0200

    fix format

--- src/structs/Validator.sol
+++ src/structs/Validator.sol
@@ -3,14 +3,14 @@ pragma solidity 0.8.19;
 
 import {FvmAddress} from "../structs/FvmAddress.sol";
 
-    struct ValidatorInfo {
-        address addr;
-        uint256 weight;
-        FvmAddress workerAddr;
-        string netAddresses;
-    }
+struct ValidatorInfo {
+    address addr;
+    uint256 weight;
+    FvmAddress workerAddr;
+    string netAddresses;
+}
 
-    struct ValidatorSet {
-        ValidatorInfo[] validators;
-        uint64 configurationNumber;
-    }
+struct ValidatorSet {
+    ValidatorInfo[] validators;
+    uint64 configurationNumber;
+}
