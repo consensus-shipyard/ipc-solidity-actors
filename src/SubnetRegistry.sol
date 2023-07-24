65079d17bc9830015180eb7236faf71c31aaeb50
cryptoAtwill <willes.lau@protocol.ai>
2023-07-24 17:19:27 +0800

    remove unused methods

--- src/SubnetRegistry.sol
+++ src/SubnetRegistry.sol
@@ -12,16 +12,6 @@ import {SubnetIDHelper} from "./lib/SubnetIDHelper.sol";
 contract SubnetRegistry {
     using SubnetIDHelper for SubnetID;
 
-    /// @notice Mapping that tracks the deployed subnet actors per user.
-    /// Key is the hash of Subnet ID, values are addresses.
-    /// mapping owner => nonce => subnet
-    mapping(address => mapping(uint64 => address)) public subnets;
-
-    /// @notice Mapping that tracks the latest nonce of the deployed
-    /// subnet for each user.
-    /// owner => nonce
-    mapping(address => uint64) public userNonces;
-
     address public immutable gateway;
 
     /// The subnet getter facet functions selectors
@@ -37,9 +27,6 @@ contract SubnetRegistry {
     error UnknownSubnet();
 
     constructor(address _gateway, bytes4[] memory _subnetGetterSelectors, bytes4[] memory _subnetManagerSelectors) {
-        if (_gateway == address(0)) {
-            revert ZeroGatewayAddress();
-        }
         gateway = _gateway;
 
         subnetGetterSelectors = _subnetGetterSelectors;
@@ -71,27 +58,6 @@ contract SubnetRegistry {
 
         subnetAddr = address(new SubnetActorDiamond(diamondCut, _params));
 
-        subnets[msg.sender][userNonces[msg.sender]] = subnetAddr;
-        ++userNonces[msg.sender];
-
         emit SubnetDeployed(subnetAddr);
-    }
-
-    /// @notice Returns the address of the latest subnet actor
-    /// deployed by a user
-    function latestSubnetDeployed(address owner) external view returns (address subnet) {
-        subnet = subnets[owner][userNonces[owner] - 1];
-        if (subnet == address(0)) {
-            revert ZeroGatewayAddress();
-        }
-    }
-
-    /// @notice Returns the address of a subnet actor deployed for a
-    /// specific nonce by a user
-    function getSubnetDeployedByNonce(address owner, uint64 nonce) external view returns (address subnet) {
-        subnet = subnets[owner][nonce];
-        if (subnet == address(0)) {
-            revert ZeroGatewayAddress();
-        }
     }
 }
