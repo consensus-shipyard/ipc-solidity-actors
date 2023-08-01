0946437f3ad42b839722de75733c19229096c142
Denis Kolegov <dnkolegov@gmail.com>
2023-08-01 20:03:14 +0200

    Update ops script

--- ops/compile-abi.sh
+++ ops/compile-abi.sh
@@ -13,6 +13,6 @@ OUTPUT=$1
 echo "[*] Compiling contracts and output core contracts ABI in $OUTPUT" 
 forge build --via-ir --sizes --skip test --out=$OUTPUT
 mkdir -p $OUTPUT
-cp $OUTPUT/SubnetActor.sol/* $OUTPUT
-cp $OUTPUT/Gateway.sol/* $OUTPUT
+cp $OUTPUT/SubnetActorDiamond.sol/* $OUTPUT
+cp $OUTPUT/GatewayDiamond.sol/* $OUTPUT
 cp $OUTPUT/SubnetRegistry.sol/* $OUTPUT
