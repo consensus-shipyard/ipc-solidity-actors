b46f5e274b038a7a47bb93d6c8f78eb797774b93
Denis Kolegov <dnkolegov@gmail.com>
2023-08-22 19:22:00 +0200

    updates paths

--- examples/contracts/TokenMessenger.sol
+++ examples/contracts/TokenMessenger.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.19;
 import {IPCAddress, SubnetID} from "../../src/structs/Subnet.sol";
 import {ZeroAddress} from "../../src/errors/IPCErrors.sol";
 import {Messenger} from "./Messenger.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "../../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 error NoTransfer();
 
