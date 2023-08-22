2edb8bfaaa24f49da88cd275381c540212a8a327
Denis Kolegov <dnkolegov@gmail.com>
2023-08-22 21:42:01 +0200

    fix script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -2,8 +2,8 @@
 # Deploys IPC on an EVM-compatible subnet using hardhat
 set -e
 
-source ./ops/deploy-libs.sh "$@"
+./ops/deploy-libs.sh "$@"
 
-source ./ops/deploy-gw.sh "$@"
+./ops/deploy-gw.sh "$@"
 
-source ./ops/deploy-reg.sh "$@"
+./ops/deploy-reg.sh "$@"
