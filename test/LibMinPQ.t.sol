7b1d02245b1a907a083d97587ede1dd7b50f421a
cryptoAtwill <willes.lau@protocol.ai>
2023-09-26 17:19:18 +0800

    fix sink

--- test/LibMinPQ.t.sol
+++ test/LibMinPQ.t.sol
@@ -22,19 +22,27 @@ contract LibMinPQTest is Test {
         validators.confirmDeposit(addr, 100);
 
         minPQ.insert(validators, addr);
-
-        console.log(minPQ.inner.posToAddress[1]);
         
         require(minPQ.getSize() == 1, "size not correct");
         (address minAddress, uint256 minValue) = minPQ.min(validators);
         require(minAddress == addr, "address not correct");
         require(minValue == 100, "min collateral correct");
+
+        addr = address(2);
+        validators.confirmDeposit(addr, 50);
+
+        minPQ.insert(validators, addr);
+
+        require(minPQ.getSize() == 2, "size not 2");
+        (minAddress, minValue) = minPQ.min(validators);
+        require(minAddress == addr, "address not 2");
+        require(minValue == 50, "min collateral 50");
     }
 
     function test_minPQInsertAndPop() public {
         require(minPQ.getSize() == 0, "initial pq size not 0");
 
-        for (uint256 i = 1; i < 100; i++) {
+        for (uint256 i = 1; i <= 100; i++) {
             address addr = address(uint160(i));
             validators.confirmDeposit(addr, 100 * i);
         }
@@ -50,14 +58,15 @@ contract LibMinPQTest is Test {
 
             require(minPQ.getSize() == size, "size not correct");
             (minAddress, minValue) = minPQ.min(validators);
+	   
             require(minAddress == addr, "address not correct");
-            require(minValue == 100 * i, "min collateral correct");
+            require(minValue == 100 * i, "min collateral not correct");
 
             size++;
         }
 
         size = 100;
-        for (uint256 i = 1; i < 100; i++) {
+        for (uint256 i = 1; i <= 100; i++) {
             address addr = address(uint160(i));
 
             (minAddress, minValue) = minPQ.min(validators);
