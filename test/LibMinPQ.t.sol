52143d782a5fb7bc2abeb0e2fb0fe05e405780b7
cryptoAtwill <willes.lau@protocol.ai>
2023-09-26 16:57:02 +0800

    more tests

--- test/LibMinPQ.t.sol
+++ test/LibMinPQ.t.sol
@@ -15,6 +15,22 @@ contract LibMinPQTest is Test {
     MinPQ private minPQ;
     ValidatorSet private validators;
 
+    function test_minPQBasic() public {
+        require(minPQ.getSize() == 0, "initial pq size not 0");
+
+        address addr = address(1);
+        validators.confirmDeposit(addr, 100);
+
+        minPQ.insert(validators, addr);
+
+        console.log(minPQ.inner.posToAddress[1]);
+        
+        require(minPQ.getSize() == 1, "size not correct");
+        (address minAddress, uint256 minValue) = minPQ.min(validators);
+        require(minAddress == addr, "address not correct");
+        require(minValue == 100, "min collateral correct");
+    }
+
     function test_minPQInsertAndPop() public {
         require(minPQ.getSize() == 0, "initial pq size not 0");
 
@@ -34,7 +50,6 @@ contract LibMinPQTest is Test {
 
             require(minPQ.getSize() == size, "size not correct");
             (minAddress, minValue) = minPQ.min(validators);
-            console.log(minAddress);
             require(minAddress == addr, "address not correct");
             require(minValue == 100 * i, "min collateral correct");
 
