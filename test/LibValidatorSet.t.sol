14eb863d6ba0426c11e884cb4510ff60ca4e4f40
cryptoAtwill <willes.lau@protocol.ai>
2023-09-28 16:57:39 +0800

    add test

--- test/LibValidatorSet.t.sol
+++ test/LibValidatorSet.t.sol
@@ -63,9 +63,10 @@ contract LibValidatorSetTest is Test {
 
         validators.confirmDeposit(address(101), 101);
 
-
-        require(!validators.activeValidators.contains(address(1)), "address 1 should not be waiting");
+        /// check waiting validator
+        require(!validators.activeValidators.contains(address(1)), "address 1 should not be active");
         require(validators.waitingValidators.contains(address(1)), "address 1 should be waiting");
+        require(validators.waitingValidators.getSize() == 1, "waiting validators should only have 1");
         
         // check new active validators
         for (uint160 i = 1; i < 100; i++) {
@@ -75,7 +76,46 @@ contract LibValidatorSetTest is Test {
         (address maxAddress, uint256 maxCollateral) = validators.activeValidators.min(validators);
         require(maxAddress == address(101), "address 101 should be the last validator");
         require(maxCollateral == 101, "address 101 collateral not 101");
+    }
 
+    /// @notice Exceeding active validator limit and there are waiting validators
+    /// Setup: 100 active validators with collateral btw 2 to 102. 5 waiting validators with collateral 1.
+    ///        Incoming validator is not active validator nor waiting validator with collateral 1000
+    /// Expected: 100 active validators, max active deposit is 1000. 6 waiting validator, collateral 1 or 2
+    function test_validatorSet_exceedingActiveLimitWithWaitingI() public {
+        // setup
+        validators.activeLimit = 100;
 
+        for (uint160 i = 2; i <= 101; i++) {
+            validators.confirmDeposit(address(i), i);
+            require(validators.isActiveValidator(address(i)), "address should be active");
+        }
+
+        for (uint160 i = 102; i <= 106; i++) {
+            validators.confirmDeposit(address(i), 1);
+            require(!validators.isActiveValidator(address(i)), "address should not be active");
+        }
+
+        // new validator
+        validators.confirmDeposit(address(1000), 1000);
+        
+        // check expected result
+
+        /// check waiting validator
+        require(validators.waitingValidators.getSize() == 6, "waiting validators should only have 6");
+        require(!validators.activeValidators.contains(address(2)), "address 2 should not be active");
+        require(validators.waitingValidators.contains(address(2)), "address 2 should be waiting");
+
+        (address maxAddress, uint256 maxCollateral) = validators.waitingValidators.max(validators);
+        require(maxAddress == address(2), "address 2 should be the max collateral validtor");
+        require(maxCollateral == 2, "address 2 should have max collateral 2");
+
+        // check new active validators
+        for (uint160 i = 1; i < 100; i++) {
+            validators.activeValidators.pop(validators);
+        }
+        (maxAddress, maxCollateral) = validators.activeValidators.min(validators);
+        require(maxAddress == address(1000), "address 1000 should be the last validator");
+        require(maxCollateral == 1000, "address 1000 collateral not 1000");
     }
 }
