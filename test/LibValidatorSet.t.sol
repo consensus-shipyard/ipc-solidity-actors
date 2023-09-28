8dee52043994e11e568eea6df6bdfba91f74b494
cryptoAtwill <willes.lau@protocol.ai>
2023-09-28 16:40:17 +0800

    add tests

--- test/LibValidatorSet.t.sol
+++ test/LibValidatorSet.t.sol
@@ -16,7 +16,7 @@ contract LibValidatorSetTest is Test {
 
     ValidatorSet private validators;
 
-    function test_noPriorActiveValidators() public {
+    function test_validatorSet_noPriorActiveValidators() public {
         validators.activeLimit = 2;
 
         validators.confirmDeposit(address(1), 50);
@@ -25,6 +25,57 @@ contract LibValidatorSetTest is Test {
         require(validators.isActiveValidator(address(1)), "address 1 should be active");
         require(validators.isActiveValidator(address(2)), "address 2 should be active");
         require(validators.waitingValidators.getSize() == 0, "waiting validators should be empty");
-        require(validators.activeValidators.getSize() == 0, "active validators size should be 2");
+        require(validators.activeValidators.getSize() == 2, "active validators size should be 2");
+    }
+
+    function test_validatorSet_activeValidatorDepositCollateral() public {
+        validators.activeLimit = 100;
+
+        for (uint160 i = 1; i <= 100; i++) {
+            validators.confirmDeposit(address(i), i);
+            require(validators.isActiveValidator(address(i)), "address should be active");
+        }
+        require(validators.waitingValidators.getSize() == 0, "waiting validators should be empty");
+        require(validators.activeValidators.getSize() == 100, "active validators size should be 100");
+
+        validators.confirmDeposit(address(1), 100);
+
+        for (uint160 i = 1; i < 100; i++) {
+            validators.activeValidators.pop(validators);
+        }
+    
+        (address maxAddress, uint256 maxCollateral) = validators.activeValidators.min(validators);
+        require(maxAddress == address(1), "address 1 should be the last validator");
+        require(maxCollateral == 101, "address 1 collateral incorrect");
+    }
+
+    /// @notice Exceeding active validator limit and there is no waiting validators
+    /// Setup: 100 active validators with collateral less than 101. New validator 
+    ///        deposits 101.
+    /// Expected: 100 active validators, max active deposit is 101. 1 waiting validator, collateral 1
+    function test_validatorSet_exceedingActiveLimitNoWaiting() public {
+        validators.activeLimit = 100;
+
+        for (uint160 i = 1; i <= 100; i++) {
+            validators.confirmDeposit(address(i), i);
+            require(validators.isActiveValidator(address(i)), "address should be active");
+        }
+
+        validators.confirmDeposit(address(101), 101);
+
+
+        require(!validators.activeValidators.contains(address(1)), "address 1 should not be waiting");
+        require(validators.waitingValidators.contains(address(1)), "address 1 should be waiting");
+        
+        // check new active validators
+        for (uint160 i = 1; i < 100; i++) {
+            validators.activeValidators.pop(validators);
+        }
+    
+        (address maxAddress, uint256 maxCollateral) = validators.activeValidators.min(validators);
+        require(maxAddress == address(101), "address 101 should be the last validator");
+        require(maxCollateral == 101, "address 101 collateral not 101");
+
+
     }
 }
