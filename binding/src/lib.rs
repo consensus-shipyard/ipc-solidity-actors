6edea362706ad0982fea658428dc8342cab9ac15
cryptoAtwill <willes.lau@protocol.ai>
2023-10-16 19:32:57 +0800

    update bindings

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -2,17 +2,17 @@
 #[macro_use]
 mod convert;
 #[allow(clippy::all)]
-pub mod gateway_diamond;
+pub mod i_diamond;
 #[allow(clippy::all)]
-pub mod gateway_getter_facet;
+pub mod gateway_diamond;
 #[allow(clippy::all)]
 pub mod gateway_manager_facet;
 #[allow(clippy::all)]
-pub mod gateway_messenger_facet;
+pub mod gateway_getter_facet;
 #[allow(clippy::all)]
 pub mod gateway_router_facet;
 #[allow(clippy::all)]
-pub mod i_diamond;
+pub mod gateway_messenger_facet;
 #[allow(clippy::all)]
 pub mod subnet_actor_diamond;
 #[allow(clippy::all)]
@@ -27,6 +27,7 @@ fvm_address_conversion!(gateway_manager_facet);
 fvm_address_conversion!(gateway_getter_facet);
 fvm_address_conversion!(gateway_router_facet);
 fvm_address_conversion!(gateway_messenger_facet);
+fvm_address_conversion!(subnet_actor_manager_facet);
 
 // The list of contracts that need to convert common types between each other
 common_type_conversion!(gateway_getter_facet, subnet_actor_manager_facet);
