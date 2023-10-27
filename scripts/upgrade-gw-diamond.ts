ace29aac774f1952d8e5b5555c6c138960f97f9c
Mikers <snissn@gmail.com>
2023-10-26 17:22:56 -1000

    work in progress upgrade code - prints out the bytecode of the local compiled contracts, also gets the active facets on the gw diamond and prints out their bytecode from the chain. todo diff and run replace on any extra

--- scripts/upgrade-gw-diamond.ts
+++ scripts/upgrade-gw-diamond.ts
@@ -3,9 +3,47 @@ import {getFacets,deployContractWithDeployer, getTransactionFees} from './util';
 
 const { getSelectors, FacetCutAction } = require('./js/diamond.js')
 
+
+const FacetNames = [
+   // 'GatewayGetterFacet',
+    'GatewayManagerFacet',
+    'GatewayRouterFacet',
+    'GatewayMessengerFacet',
+    'DiamondLoupeFacet',
+    'DiamondCutFacet'
+]
+
+
 async function upgradeSubnetActorDiamond (gatewayDiamondAddress: string, libs: { [key in string]: string }) {
-   const x = await getFacets("0x11fae1c6ab8d1ec073d60b370232f44284687dc7")
-   console.log(x)
+   const facets = await getFacets("0x5B509997C12098c908A5B160D4D25b645AB53343")
+   console.log(facets)
+   const provider = ethers.provider;
+
+   for (let facetIndex in FacetNames){
+    const facetName = FacetNames[facetIndex];
+    console.log(facetName)
+    const fs = require('fs');
+    const path = require('path');
+    const facetJsonPath = path.join(__dirname, '..', 'out', `${facetName}.sol`, `${facetName}.json`);
+    const facetJson = fs.readFileSync(facetJsonPath, 'utf8');
+    const facetBytecode = JSON.parse(facetJson).deployedBytecode.object;
+     console.log(facetName, facetBytecode);
+
+   }
+
+   for (let contractAddress in facets) {
+    try {
+      // Fetch the bytecode
+      const bytecode = await provider.getCode(contractAddress);
+
+      // Log the bytecode to the console
+      console.log(`Bytecode for ${contractAddress}:`, bytecode);
+    } catch (error) {
+      // Print any errors to stderr
+      console.error(`Error fetching bytecode for ${contractAddress}:`, error.message);
+    }
+  }
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
