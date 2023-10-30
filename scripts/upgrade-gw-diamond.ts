0a591c9a03e3adade04fee399fb65a1d79f0e196
Mikers <snissn@gmail.com>
2023-10-30 10:25:17 -1000

    prettier

--- scripts/upgrade-gw-diamond.ts
+++ scripts/upgrade-gw-diamond.ts
@@ -1,79 +1,110 @@
-import hre, { ethers } from "hardhat";
-import {getFacets,deployContractWithDeployer, getTransactionFees} from './util';
+import hre, { ethers } from 'hardhat'
+import {
+    getFacets,
+    deployContractWithDeployer,
+    getTransactionFees,
+} from './util'
 
 const { getSelectors, FacetCutAction } = require('./js/diamond.js')
 
-
 const FacetNames = [
-   // 'GatewayGetterFacet',
+    // 'GatewayGetterFacet',
     'GatewayManagerFacet',
     'GatewayRouterFacet',
     'GatewayMessengerFacet',
     'DiamondLoupeFacet',
-    'DiamondCutFacet'
+    'DiamondCutFacet',
 ]
 
-// given a facet address and a gateway diamond, 
+// given a facet address and a gateway diamond,
 // upgrade the diamond to use the new facet
-async function upgradeSubnetActorDiamond (gatewayAddress: string, replacementFacetName: string, facetLibs: { [key in string]: string }) {
-  
-  if (!gatewayAddress) throw new Error(`Gateway is missing`);
-  if (!facetLibs || Object.keys(facetLibs).length === 0) throw new Error(`Libraries are missing`);
+async function upgradeSubnetActorDiamond(
+    gatewayAddress: string,
+    replacementFacetName: string,
+    facetLibs: { [key in string]: string },
+) {
+    if (!gatewayAddress) throw new Error(`Gateway is missing`)
+    if (!facetLibs || Object.keys(facetLibs).length === 0)
+        throw new Error(`Libraries are missing`)
 
-  await hre.run('compile');
-  const [deployer] = await ethers.getSigners();
-  const txArgs = await getTransactionFees();
+    await hre.run('compile')
+    const [deployer] = await ethers.getSigners()
+    const txArgs = await getTransactionFees()
 
-  let replacementFacet = await deployContractWithDeployer(
-    deployer,
-    replacementFacetName,
-    facetLibs, txArgs
-);
-await replacementFacet.deployed();
+    let replacementFacet = await deployContractWithDeployer(
+        deployer,
+        replacementFacetName,
+        facetLibs,
+        txArgs,
+    )
+    await replacementFacet.deployed()
 
-  const gateway = await ethers.getContractAt("Gateway", gatewayAddress, deployer);
-  const facetCuts = [{
-    facetAddress: replacementFacet.address,
-    action: FacetCutAction.Replace,
-    functionSelectors: getSelectors(replacementFacet)
-  }]
-  const diamondCutter = await ethers.getContractAt("DiamondCutFacet", gatewayAddress, deployer);
+    const gateway = await ethers.getContractAt(
+        'Gateway',
+        gatewayAddress,
+        deployer,
+    )
+    const facetCuts = [
+        {
+            facetAddress: replacementFacet.address,
+            action: FacetCutAction.Replace,
+            functionSelectors: getSelectors(replacementFacet),
+        },
+    ]
+    const diamondCutter = await ethers.getContractAt(
+        'DiamondCutFacet',
+        gatewayAddress,
+        deployer,
+    )
 
-  // 0x0 (contract address) and "" (call data) can be used to send call data to contract
-  diamondCutter.diamondCut(facetCuts, "0x0000000000000000000000000000000000000000", "");
-
+    // 0x0 (contract address) and "" (call data) can be used to send call data to contract
+    diamondCutter.diamondCut(
+        facetCuts,
+        '0x0000000000000000000000000000000000000000',
+        '',
+    )
 }
 
-async function scratch_upgradeSubnetActorDiamond (gatewayDiamondAddress: string, libs: { [key in string]: string }) {
-   const facets = await getFacets("0x5B509997C12098c908A5B160D4D25b645AB53343")
-   console.log(facets)
-   const provider = ethers.provider;
+async function scratch_upgradeSubnetActorDiamond(
+    gatewayDiamondAddress: string,
+    libs: { [key in string]: string },
+) {
+    const facets = await getFacets('0x5B509997C12098c908A5B160D4D25b645AB53343')
+    console.log(facets)
+    const provider = ethers.provider
 
-   for (let facetIndex in FacetNames){
-    const facetName = FacetNames[facetIndex];
-    console.log(facetName)
-    const fs = require('fs');
-    const path = require('path');
-    const facetJsonPath = path.join(__dirname, '..', 'out', `${facetName}.sol`, `${facetName}.json`);
-    const facetJson = fs.readFileSync(facetJsonPath, 'utf8');
-    const facetBytecode = JSON.parse(facetJson).deployedBytecode.object;
-     console.log(facetName, facetBytecode);
-
-   }
-
-   for (let contractAddress in facets) {
-    try {
-      // Fetch the bytecode
-      const bytecode = await provider.getCode(contractAddress);
-
-      // Log the bytecode to the console
-      console.log(`Bytecode for ${contractAddress}:`, bytecode);
-    } catch (error) {
-      // Print any errors to stderr
-      console.error(`Error fetching bytecode for ${contractAddress}:`, error.message);
+    for (let facetIndex in FacetNames) {
+        const facetName = FacetNames[facetIndex]
+        console.log(facetName)
+        const fs = require('fs')
+        const path = require('path')
+        const facetJsonPath = path.join(
+            __dirname,
+            '..',
+            'out',
+            `${facetName}.sol`,
+            `${facetName}.json`,
+        )
+        const facetJson = fs.readFileSync(facetJsonPath, 'utf8')
+        const facetBytecode = JSON.parse(facetJson).deployedBytecode.object
+        console.log(facetName, facetBytecode)
     }
-  }
 
+    for (let contractAddress in facets) {
+        try {
+            // Fetch the bytecode
+            const bytecode = await provider.getCode(contractAddress)
+
+            // Log the bytecode to the console
+            console.log(`Bytecode for ${contractAddress}:`, bytecode)
+        } catch (error) {
+            // Print any errors to stderr
+            console.error(
+                `Error fetching bytecode for ${contractAddress}:`,
+                error.message,
+            )
+        }
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -81,11 +112,10 @@ async function scratch_upgradeSubnetActorDiamond (gatewayDiamondAddress: string,
 if (require.main === module) {
     upgradeSubnetActorDiamond()
         .then(() => process.exit(0))
-        .catch(error => {
+        .catch((error) => {
             console.error(error)
             process.exit(1)
         })
 }
 
 exports.upgradeDiamond = upgradeSubnetActorDiamond
-
