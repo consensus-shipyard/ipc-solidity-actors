abb455b60f1760b98678fbc955cc85ecab0ddedd
mike seiler <snissn@gmail.com>
2023-10-31 13:26:41 -0700

    clean up format

--- scripts/upgrade-gw-diamond.ts
+++ scripts/upgrade-gw-diamond.ts
@@ -1,59 +1,53 @@
 import hre, { ethers } from 'hardhat'
-import { DiamondLoupeFacet__factory } from '../typechain/factories/DiamondLoupeFacet__factory';
-import { GatewayMessengerFacet__factory } from '../typechain/factories/GatewayMessengerFacet__factory';
+import { DiamondLoupeFacet__factory } from '../typechain/factories/DiamondLoupeFacet__factory'
+import { GatewayMessengerFacet__factory } from '../typechain/factories/GatewayMessengerFacet__factory'
 import {
     getFacets,
     deployContractWithDeployer,
     getTransactionFees,
 } from './util'
-import solc from 'solc';
-import { findLinkReferences } from 'solc';
-import * as linker from 'solc/linker';
-
-
-
-
+import solc from 'solc'
+import { findLinkReferences } from 'solc'
+import * as linker from 'solc/linker'
 
 const lazyImport = async (module: any) => {
-  return await import(module);
-};
-
+    return await import(module)
+}
 
 const { getSelectors, FacetCutAction } = require('./js/diamond.js')
 
-const fs = require('fs');
+const fs = require('fs')
 
 function getBytecode(fileName) {
-  try {
-    // Read the file synchronously
-    const fileContent = fs.readFileSync(fileName, 'utf8');
-    
-    // Split the file content into lines
-    const lines = fileContent.split('\n');
-    
-    // Initialize a flag to identify when the target line is found
-    let found = false;
-    
-    for (const line of lines) {
-      // If the previous line was the target line, return the current line
-      if (found) {
-        // Trim semicolons and quotes from the beginning and end of the string
-        return line.trim().replace(/^[";]+|[";]+$/g, '');
-      }
-      
-      // Check if the current line is the target line
-      if (line.includes('const _bytecode =')) {
-        found = true;
-      }
-    }
-    
-    // If the loop completes without returning, the target line was not found
-    throw new Error('Target line "const _bytecode =" not found in the file');
-  } catch (error) {
-    console.error('Error reading file:', error.message);
-  }
-}
+    try {
+        // Read the file synchronously
+        const fileContent = fs.readFileSync(fileName, 'utf8')
 
+        // Split the file content into lines
+        const lines = fileContent.split('\n')
+
+        // Initialize a flag to identify when the target line is found
+        let found = false
+
+        for (const line of lines) {
+            // If the previous line was the target line, return the current line
+            if (found) {
+                // Trim semicolons and quotes from the beginning and end of the string
+                return line.trim().replace(/^[";]+|[";]+$/g, '')
+            }
+
+            // Check if the current line is the target line
+            if (line.includes('const _bytecode =')) {
+                found = true
+            }
+        }
+
+        // If the loop completes without returning, the target line was not found
+        throw new Error('Target line "const _bytecode =" not found in the file')
+    } catch (error) {
+        console.error('Error reading file:', error.message)
+    }
+}
 
 const FacetNames = [
     // 'GatewayGetterFacet',
@@ -74,7 +68,7 @@ async function _upgradeSubnetActorDiamond(
     if (!gatewayAddress) throw new Error(`Gateway is missing`)
 
     //if (!facetLibs || Object.keys(facetLibs).length === 0)
-        //throw new Error(`Libraries are missing`)
+    //throw new Error(`Libraries are missing`)
 
     await hre.run('compile')
     const [deployer] = await ethers.getSigners()
@@ -114,21 +108,57 @@ async function _upgradeSubnetActorDiamond(
     )
 }
 
-async function upgradeSubnetActorDiamond(
-    deployments
-) {
-    const gatewayDiamondAddress = deployments.Gateway
-    const [deployer] = await ethers.getSigners();
+async function generateBytecode(facet) {
+    const facetName = facet.name
+    const libs = facet.libs
+    const bytecodeNeedsLink = getBytecode(
+        `./typechain/factories/${facetName}__factory.ts`,
+    )
+    let libs2 = {}
+    // Loop through each key in the libs
+    for (let key in libs) {
+        let newKey = `src/lib/${key}.sol:${key}`
+        libs2[newKey] = libs[key]
+    }
 
+    // Link the bytecode with the libraries
+    const bytecode = linker.linkBytecode(bytecodeNeedsLink, libs2)
+    const provider = new ethers.providers.JsonRpcProvider(
+        'http://127.0.0.1:8545',
+    )
+
+    // Create a wallet
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+    // Create a contract factory with a dummy ABI
+    const contractFactory = new ethers.ContractFactory([], bytecode, wallet)
+
+    // Deploy the contract
+    const contract = await contractFactory.deploy()
+    await contract.deployed()
+
+    // Get the latest bytecode of the contract
+    const bytecodeLatest = await provider.getCode(contract.address)
+    return bytecodeLatest
+}
+
+// Function to upgrade the Subnet Actor Diamond
+async function upgradeSubnetActorDiamond(deployments) {
+    // Get the Gateway Diamond address from the deployments
+    const gatewayDiamondAddress = deployments.Gateway
+
+    // Get the facets of the Gateway Diamond
     const facets = await getFacets(gatewayDiamondAddress)
-    console.log(facets)
     const provider = ethers.provider
 
+    const deployedBytecode = {}
+
+    // Loop through each contract address in the facets
     for (let contractAddress in facets) {
         try {
-            // Fetch the bytecode
+            // Fetch the bytecode of the contract
             const bytecode = await provider.getCode(contractAddress)
-
+            deployedBytecode[bytecode] = contractAddress
             // Log the bytecode to the console
             console.log(`Bytecode for ${contractAddress}:\n`, bytecode)
         } catch (error) {
@@ -140,112 +170,16 @@ async function upgradeSubnetActorDiamond(
         }
     }
 
-
+    // Loop through each facet in the deployments
     for (let facetIndex in deployments.Facets) {
         const facet = deployments.Facets[facetIndex]
-        const facetName = facet.name
-        const libs = facet.libs
-   const bytecodeNeedsLink = getBytecode( `./typechain/factories/${facetName}__factory.ts`);
-   //const linkLibraryAddresses: GatewayMessengerFacetLibraryAddresses = {
-         //"SubnetIDHelper": "0x116d795e669fd39aB4E91068e74B182434af503A",
-           //"StorableMsgHelper": "0xbce050A3c7c39Be710beBaD368A4F0A1CdffAA39"
-   //};
-//
-   //const constructorParams: GatewayMessengerFacetConstructorParams = [linkLibraryAddresses];
-//
-   //const res = GatewayMessengerFacet__factory.linkBytecode(constructorParams);
-
-   //console.log(res);
-
-   let libs2 = {};
-
-   for (let key in libs) {
-           let newKey = `src/lib/${key}.sol:${key}`;
-               libs2[newKey] = libs[key];
-   }
-
-   console.log(libs2);
-
-   //const bytecode = linker.linkBytecode(bytecodeNeedsLink, Object.entries(libs).reduce((acc, [key, value]) => ({ ...acc, [`src/lib/${key}.sol:${key}`]: value }), {}));
-
-   const bytecode = linker.linkBytecode(bytecodeNeedsLink, libs2);
-   const linkReferences = linker.findLinkReferences(bytecode);
-   console.log(linkReferences)
-   //
-
-        //
-//console.log('linking',facetName)
-////console.log('linking',libs)
-    //console.log(linkReferences);
-    console.log(bytecode);
-//
-
-var i =0
-console.log(i++)
-const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
-
-console.log(i++)
-// Create a wallet
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
-console.log(i++)
-// Create a contract factory with a dummy ABI
-const contractFactory = new ethers.ContractFactory([], bytecode, wallet);
-
-console.log(i++)
-// Deploy the contract
-const contract = await contractFactory.deploy();
-
-console.log(i++)
-// Log the contract address
-console.log('Contract deployed to address:', contract.address);
-console.log('Contract deployed to address:', contract);
-
-// Log the transaction hash
-console.log('Transaction hash:', contract.deployTransaction.hash);
-
-// Wait for the contract to be mined
-await contract.deployed();
-
-// To print the contract details in a valid JSON format:
-console.log(JSON.stringify({
-      contractAddress: contract.address,
-        transactionHash: contract.deployTransaction.hash,
-          // ... any other details you want to include
-}, null, 2));
-
-
-// To print the contract details in a valid JSON format:
-console.log(JSON.stringify(contract));
-
-
-
-            const bytecodeLatest = await provider.getCode(contract.address)
-            console.log("bytecodeLatest")
-            console.log(bytecodeLatest)
-
-        //console.log(facetName, libs)
-        const contractFactory2 = await ethers.getContractFactory(facetName, { signer: deployer, libraries: libs, });
-        const c = contractFactory2.attach(contract.address)
-        console.log("factory attach:")
-        console.log(c)
-        //console.log(facetName)
-        //console.log(contractFactory.bytecode)
-
-        // const fs = require('fs')
-        // const path = require('path')
-        // const facetJsonPath = path.join(
-        //     __dirname,
-        //     '..',
-        //     'out',
-        //     `${facetName}.sol`,
-        //     `${facetName}.json`,
-        // )
-        // const facetJson = fs.readFileSync(facetJsonPath, 'utf8')
-        // const facetBytecode = JSON.parse(facetJson).deployedBytecode.object
-        // console.log(facetName, facetBytecode)
+        const facetBytecode = await generateBytecode(facet)
+        if (!deployedBytecode.hasOwnProperty(facetBytecode)) {
+            console.info(
+                `Facet bytecode not found in deployed bytecode: ${facet}`,
+            )
+        }
     }
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
