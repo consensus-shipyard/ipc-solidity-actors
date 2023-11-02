f466899657c862e4bc7ed29b5ef83e6eef9d008b
mike seiler <snissn@gmail.com>
2023-11-02 15:32:31 -0700

    apply diff and run remove, update and add to support contracts changing shape

--- scripts/util.ts
+++ scripts/util.ts
@@ -209,38 +209,66 @@ function filterSelectors(input) {
 }
 
 function compareArrays(onChain, newArr) {
-  const result = {
-    removedSelectors: [],
-    matchingSelectors: [],
-    addedSelectors: []
-  };
-
-  // Create a Map for easier lookup
-  const onChainMap = new Map();
-  onChain.forEach(selector => onChainMap.set(selector, true));
-
-  const newArrMap = new Map();
-  newArr.forEach(selector => newArrMap.set(selector, true));
-
-  // Find matching and removed selectors
-  onChain.forEach(selector => {
-    if (newArrMap.has(selector)) {
-      result.matchingSelectors.push(selector);
-    } else {
-      result.removedSelectors.push(selector);
+    const result = {
+        removedSelectors: [],
+        matchingSelectors: [],
+        addedSelectors: [],
     }
-  });
 
-  // Find added selectors
-  newArr.forEach(selector => {
-    if (!onChainMap.has(selector)) {
-      result.addedSelectors.push(selector);
-    }
-  });
+    // Create a Map for easier lookup
+    const onChainMap = new Map()
+    onChain.forEach((selector) => onChainMap.set(selector, true))
 
-  return result;
+    const newArrMap = new Map()
+    newArr.forEach((selector) => newArrMap.set(selector, true))
+
+    // Find matching and removed selectors
+    onChain.forEach((selector) => {
+        if (newArrMap.has(selector)) {
+            result.matchingSelectors.push(selector)
+        } else {
+            result.removedSelectors.push(selector)
+        }
+    })
+
+    // Find added selectors
+    newArr.forEach((selector) => {
+        if (!onChainMap.has(selector)) {
+            result.addedSelectors.push(selector)
+        }
+    })
+
+    return result
 }
+async function cutFacetOnChain(
+    diamondAddress: string,
+    replacementFacet: any,
+    action,
+    functionSelectors,
+) {
+    const [deployer] = await ethers.getSigners()
+    const txArgs = await getTransactionFees()
 
+    const facetCuts = [
+        {
+            facetAddress: replacementFacet.address,
+            action: action,
+            functionSelectors: functionSelectors,
+        },
+    ]
+    const diamondCutter = await ethers.getContractAt(
+        'DiamondCutFacet',
+        diamondAddress,
+        deployer,
+    )
+    const tx = await diamondCutter.diamondCut(
+        facetCuts,
+        ethers.constants.AddressZero,
+        ethers.utils.formatBytes32String(''),
+        txArgs,
+    )
+    await tx.wait()
+}
 
 // given a facet address and a diamond address,
 // upgrade the diamond to use the new facet
@@ -273,33 +301,31 @@ Replacement Facet Name: ${replacementFacetName}
 
     console.log('on chain selectors:')
     console.log(onChainFunctionSelectors)
-    console.log("new selectors:")
-    const result = compareArrays(onChainFunctionSelectors, filterSelectors(getSelectors(replacementFacet)))
-    console.log("diff:")
-    console.log(result)
-
-    const facetCuts = [
-        {
-            facetAddress: replacementFacet.address,
-            action: FacetCutAction.Replace,
-            functionSelectors: filterSelectors(getSelectors(replacementFacet)),
-        },
-    ]
-    const diamondCutter = await ethers.getContractAt(
-        'DiamondCutFacet',
-        diamondAddress,
-        deployer,
+    console.log('new selectors:')
+    const result = compareArrays(
+        onChainFunctionSelectors,
+        filterSelectors(getSelectors(replacementFacet)),
     )
-    // 0x0 (contract address) and "" (call data) can be used to send call data to contract
-    const tx = await diamondCutter.diamondCut(
-        facetCuts,
-        ethers.constants.AddressZero,
-        ethers.utils.formatBytes32String(''),
-        txArgs,
-    )
-    await tx.wait()
-    console.log("name is, ", replacementFacetName)
-    console.log("ret is", { [replacementFacetName]: replacementFacet.address });
-    return { [replacementFacetName]: replacementFacet.address };
 
+    async function cutSelectorsOnChain(action, selectors) {
+        if (selectors.length > 0) {
+            await cutFacetOnChain(
+                diamondAddress,
+                replacementFacet.address,
+                action,
+                selectors,
+            )
+        }
+    }
+
+    // cut changes for each facet cut action - remove replace and add
+    await cutSelectorsOnChain(FacetCutAction.Remove, result['removedSelectors'])
+    await cutSelectorsOnChain(
+        FacetCutAction.Replace,
+        result['matchingSelectors'],
+    )
+    await cutSelectorsOnChain(FacetCutAction.Add, result['addedSelectors'])
+
+    //end move facet
+    return { [replacementFacetName]: replacementFacet.address }
 }
