7c74e80b3cd8c97291eb055bac8a6e5f72a88593
Denis Kolegov <dnkolegov@gmail.com>
2023-11-13 20:15:10 +0300

    nindings

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -2,21 +2,17 @@
 #[macro_use]
 mod convert;
 #[allow(clippy::all)]
-pub mod gateway_diamond;
+pub mod i_diamond;
 #[allow(clippy::all)]
-pub mod gateway_getter_facet;
+pub mod gateway_diamond;
 #[allow(clippy::all)]
 pub mod gateway_manager_facet;
 #[allow(clippy::all)]
-pub mod gateway_messenger_facet;
+pub mod gateway_getter_facet;
 #[allow(clippy::all)]
 pub mod gateway_router_facet;
 #[allow(clippy::all)]
-pub mod i_diamond;
-#[allow(clippy::all)]
-pub mod lib_staking;
-#[allow(clippy::all)]
-pub mod lib_staking_change_log;
+pub mod gateway_messenger_facet;
 #[allow(clippy::all)]
 pub mod subnet_actor_diamond;
 #[allow(clippy::all)]
@@ -25,6 +21,10 @@ pub mod subnet_actor_getter_facet;
 pub mod subnet_actor_manager_facet;
 #[allow(clippy::all)]
 pub mod subnet_registry;
+#[allow(clippy::all)]
+pub mod lib_staking;
+#[allow(clippy::all)]
+pub mod lib_staking_change_log;
 
 // The list of contracts need to convert FvmAddress to fvm_shared::Address
 fvm_address_conversion!(gateway_manager_facet);
