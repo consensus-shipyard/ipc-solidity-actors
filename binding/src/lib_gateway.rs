5b5ad51d06de287ad9fc6ad557d0b2bf7d1b56f7
auto-commit <auto-commit@users.noreply.github.com>
2023-11-29 17:27:48 +0000

    commit rust binding

--- binding/src/lib_gateway.rs
+++ binding/src/lib_gateway.rs
@@ -132,13 +132,13 @@ pub mod lib_gateway {
         __abi,
     );
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`\x80\x80`@R4`\x17W`:\x90\x81`\x1D\x8290\x81PP\xF3[`\0\x80\xFD\xFE`\0\x80\xFD\xFE\xA2dipfsX\"\x12 G~V0\xAE\xD8\xA0\xAC`\xAC\xF9\x1C\x0F\xD2\xA2$\x07\xEA\xD2\x03'\xCAU\xB1\xAB\xF26\x89\x90X!\x0BdsolcC\0\x08\x13\x003";
+    const __BYTECODE: &[u8] = b"`\x80\x80`@R4`\x17W`:\x90\x81`\x1D\x8290\x81PP\xF3[`\0\x80\xFD\xFE`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xBF|\x7F\xD6rr\xF2\x049\xA4\xE5\x84n\x99\x1E\x8E\xB9\xD7&\xCB\xBCScw<V\x8F eXM/dsolcC\0\x08\x13\x003";
     /// The bytecode of the contract.
     pub static LIBGATEWAY_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __BYTECODE,
     );
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"`\0\x80\xFD\xFE\xA2dipfsX\"\x12 G~V0\xAE\xD8\xA0\xAC`\xAC\xF9\x1C\x0F\xD2\xA2$\x07\xEA\xD2\x03'\xCAU\xB1\xAB\xF26\x89\x90X!\x0BdsolcC\0\x08\x13\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xBF|\x7F\xD6rr\xF2\x049\xA4\xE5\x84n\x99\x1E\x8E\xB9\xD7&\xCB\xBCScw<V\x8F eXM/dsolcC\0\x08\x13\x003";
     /// The deployed bytecode of the contract.
     pub static LIBGATEWAY_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
         __DEPLOYED_BYTECODE,
