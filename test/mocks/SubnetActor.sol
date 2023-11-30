19a44890a8338b3f2356f056332b686d6517ae26
Denis Kolegov <dnkolegov@gmail.com>
2023-11-30 22:34:26 +0100

    Registry invariant

--- test/mocks/SubnetActor.sol
+++ test/mocks/SubnetActor.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {SubnetActorManagerFacet} from "../../src/subnet/SubnetActorManagerFacet.sol";
 import {LibStaking} from "../../src/lib/LibStaking.sol";
 
-contract SubnetManagerTestUtil is SubnetActorManagerFacet {
+contract SubnetActorManagerFacetMock is SubnetActorManagerFacet {
     function confirmChange(uint64 _configurationNumber) external {
         LibStaking.confirmChange(_configurationNumber);
     }
