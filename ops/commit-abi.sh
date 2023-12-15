b2e8c121b10a4b220a3044233d30ef40744e0142
Alfonso de la Rocha <adlrocha@tutamail.com>
2023-12-15 18:39:40 +0100

    IPC-318: more files that were lost in the merge

--- ops/commit-abi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# checks and commit changes in output artifacts
-if [[ `git status ./out --porcelain` ]]; then
-    echo "********** NOT ALL ABI ARTIFACTS ARE COMMITTED, AUTO PUSH **********\n";
-    git add ./out
-    git commit -m "commit ABI artifacts"
-    git push
-fi;
