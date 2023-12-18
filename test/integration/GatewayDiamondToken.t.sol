75966e28f3e799b4e9f5a89fb543854080f2ee85
raulk <raul.kripalani@gmail.com>
2023-12-18 19:02:34 +0100

    add more ERC20 tests; add TODO placeholders.

--- test/integration/GatewayDiamondToken.t.sol
+++ test/integration/GatewayDiamondToken.t.sol
@@ -47,7 +47,7 @@ import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {ERC20PresetFixedSupply} from "../helpers/ERC20PresetFixedSupply.sol";
 import {IERC20Errors} from "openzeppelin-contracts/interfaces/draft-IERC6093.sol";
 
-contract GatewayActorDiamondTest is Test, IntegrationTestBase {
+contract GatewayDiamondTokenTest is Test, IntegrationTestBase {
     using SubnetIDHelper for SubnetID;
     using CrossMsgHelper for CrossMsg;
     using StorableMsgHelper for StorableMsg;
@@ -61,13 +61,29 @@ contract GatewayActorDiamondTest is Test, IntegrationTestBase {
         token = new ERC20PresetFixedSupply("TestToken", "TEST", 1_000_000, address(this));
     }
 
-    function test_revertConstruction() public {
+    function test_fundWithToken_NativeSupply_Reverts() public {
+        (address validatorAddress, bytes memory publicKey) = TestUtils.deriveValidatorAddress(100);
+        join(validatorAddress, publicKey);
+
         address caller = vm.addr(1);
         vm.deal(caller, 100);
 
         (SubnetID memory subnetId, , , , , ) = getSubnet(address(saManager));
+
+        vm.prank(caller);
         vm.expectRevert(SupplySourceHelper.UnexpectedSupplySource.selector);
         gwManager.fundWithToken(subnetId, FvmAddressHelper.from(caller), 100);
+    }
+
+    function test_fund_TokenSupply_Reverts() public {
+        address caller = vm.addr(1);
+        vm.deal(caller, 100);
+
+        Subnet memory subnet = createTokenSubnet(address(token));
+
+        vm.prank(caller);
+        vm.expectRevert(SupplySourceHelper.UnexpectedSupplySource.selector);
+        gwManager.fund{value: 100}(subnet.id, FvmAddressHelper.from(caller));
     }
 
     function testFail_InexistentToken() public {
@@ -76,39 +92,61 @@ contract GatewayActorDiamondTest is Test, IntegrationTestBase {
         createTokenSubnet(addr);
     }
 
-    function test_fundWithToken() public {
-        createTokenSubnet(address(token));
-
-        address caller = vm.addr(1);
-        vm.deal(caller, 100);
+    function test_fundWithToken_FailsInsufficientBalance() public {
+        Subnet memory subnet = createTokenSubnet(address(token));
 
         // account has native balance but no tokens, reverts.
-        (SubnetID memory subnetId, , , , , ) = getSubnet(address(saManager));
+        address caller = vm.addr(1);
+        vm.deal(caller, 100);
         vm.prank(caller);
         vm.expectRevert(
             abi.encodeWithSelector(IERC20Errors.ERC20InsufficientAllowance.selector, address(gatewayDiamond), 0, 1)
         );
-        gwManager.fundWithToken(subnetId, FvmAddressHelper.from(caller), 1);
+        gwManager.fundWithToken(subnet.id, FvmAddressHelper.from(caller), 1);
+    }
 
-        // ghive the caller some funds
+    function test_fundWithToken_Succeeds() public {
+        Subnet memory subnet = createTokenSubnet(address(token));
+
+        address caller = vm.addr(1);
+        vm.deal(caller, 100);
         token.transfer(caller, 100);
-
         vm.startPrank(caller);
+
+        // caller approves the gateway to spend funds on their behalf.
         token.approve(address(gatewayDiamond), 100);
 
-        // now succeeds.
+        // funding now succeeds.
         vm.expectEmit(true, false, false, false, address(gatewayDiamond));
         // TODO check value, address, etc.
         CrossMsg memory dummy;
         emit LibGateway.NewTopDownMessage(address(saDiamond), dummy);
-        gwManager.fundWithToken(subnetId, FvmAddressHelper.from(caller), 1);
+        gwManager.fundWithToken(subnet.id, FvmAddressHelper.from(caller), 1);
 
-        (, , uint256 nonce, , uint256 circSupply, ) = getSubnet(address(saManager));
-        assertEq(circSupply, 1);
-        assertEq(nonce, 1);
+        (, Subnet memory subnetAfter) = gwGetter.getSubnet(subnet.id);
+        assertEq(subnetAfter.circSupply, 1);
+        assertEq(subnetAfter.topDownNonce, 1);
     }
 
-    function createTokenSubnet(address tokenAddress) internal {
+    function test_withdrawal() public {
+        // TODO:
+        // 1. Test successful withdrawal.
+        // 2. Test withdrawal attempt beyond subnet's circulating supply.
+    }
+
+    function test_generalMessagePassing() public {
+        // TODO:
+        // 1. Test ERC20 that child-to-parent GMP where child is ERC20 results in transfer + call.
+    }
+
+    function test_propagation() public {
+        // TODO:
+        // 1. Test that propagation is rejected when sender is ERC20.
+        // 2. Test that propagation is rejected when receiver is ERC20.
+        // 3. Test that propagation is rejected when an intermediary subnet is ERC20.
+    }
+
+    function createTokenSubnet(address tokenAddress) internal returns (Subnet memory) {
         // Create a subnet actor in the root network, with an ERC20 supply source with the specified token address.
         SubnetActorDiamond.ConstructorParams memory saConstructorParams = defaultSubnetActorParamsWithGateway(
             address(gatewayDiamond)
@@ -126,5 +164,11 @@ contract GatewayActorDiamondTest is Test, IntegrationTestBase {
 
         (address validatorAddress, bytes memory publicKey) = TestUtils.deriveValidatorAddress(100);
         join(validatorAddress, publicKey);
+
+        SubnetID memory subnetId = gwGetter.getNetworkName().createSubnetId(address(saDiamond));
+
+        (bool exists, Subnet memory subnet) = gwGetter.getSubnet(subnetId);
+        assert(exists);
+        return subnet;
     }
 }
